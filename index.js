444004ff46e6f287849233b7e22fab096543ec8d
Moosadeveloper <janmoosa77@gmail.com>
2024-06-07 19:51:47 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var s=10;
+var d=5;
+s=s-5;
+d=d+5;
+  let e=30;
+  let x=50;
+  if(e>20 && x>30){
+    alert("done")
+  }
+
+  var s=prompt("How are you","fine");
+  var d=prompt("what is your name");
+  if(s==="fine"&& d==="ali"){
+    alert("wellcome")
+  }else{
+    alert("Please Try again")
+  }
